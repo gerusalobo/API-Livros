ebd18138f0eae309c9b5a305674ca946113e2978
gerusalobo <gerusalobo@gmail.com>
2024-07-24 12:00:39 -0300

    Update livroRoutes.js

--- Crud_MongoDB/routes/livroRoutes.js
+++ Crud_MongoDB/routes/livroRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const livroController = require('../controllers/livroController');
+const livroController = require('../controllers/livrocontroller');
 
 /**
  * @swagger
